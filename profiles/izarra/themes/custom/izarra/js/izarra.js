aa5067dafb51dbdeff2f97ae811511af2ccdf189
sographik <sographik@gmail.com>
2014-04-24 17:17:29 +0200

    Création du fichier izarra.js et déclaration dans le thème + positionnement css

---
+++ profiles/izarra/themes/custom/izarra/js/izarra.js
@@ -0,0 +1,23 @@
+/*
+function izarra_slider(){
+        var largeur = jQuery(".view-slides.view-display-id-page").css("width");
+        var hauteur = jQuery("#region-content").css("height");
+        jQuery(".views-field-field-bg-image .field-content").height(hauteur);
+        jQuery(".views-field-field-bg-image .field-content").width("100%");
+        jQuery(".views-field-field-bg-image .field-content").css('overflow','hidden');
+        jQuery(".views-field-field-bg-image .field-content").css('position','relative');
+        jQuery(".views-field-field-bg-image img").height(hauteur);
+        jQuery(".views-field-field-bg-image img").css({position: "absolute",display:"block",top:"0",width:"auto"});
+        jQuery("#region-content").css('min-height',hauteur);
+        }
+
+jQuery(document).ready(function () {
+    izarra_slider();//run when page first loads
+});
+
+jQuery(window).resize(function () {
+    izarra_slider();//run on every window resize
+});
+
+(jQuery);
+*/
